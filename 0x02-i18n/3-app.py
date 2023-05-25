a9d657ad53b1f8284e5e47d9a4357ec8171f70de
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-05-25 13:57:57 +0100

    Changed the rendered file

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -25,7 +25,7 @@ def get_locale():
 @app.route("/")
 def display_basic_page():
     """Displays a simple page to test Babel connection"""
-    return render_template("2-index.html")
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
