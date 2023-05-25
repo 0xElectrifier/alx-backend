249a0ae52e28807113c735e1b400d2bd982f0f9a
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-05-25 13:43:30 +0100

    Task 3 Done: Parameterising templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,44 +1,32 @@
 #!/usr/bin/env python3
-"""
-Flask app
-"""
-from flask import (
-    Flask,
-    render_template,
-    request
-)
+"""Module for Parameterising Templates"""
+from flask import (render_template, request, Flask)
 from flask_babel import Babel
 
 
-class Config(object):
-    """
-    Configuration for Babel
-    """
+class Config:
+    """Configures available languages in our app"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-app.config.from_object(Config)
 babel = Babel(app)
+app.config.from_object(Config)
 
 
 @babel.localeselector
 def get_locale():
-    """
-    Select and return best language match based on supported languages
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """Returns the locale from request"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Handles / route
-    """
-    return render_template('3-index.html')
+@app.route("/")
+def display_basic_page():
+    """Displays a simple page to test Babel connection"""
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run()
