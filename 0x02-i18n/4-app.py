02358f22e81e774cd7dbad198a148630597caff4
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-05-25 14:51:00 +0100

    Forcing locale with URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,47 +1,36 @@
 #!/usr/bin/env python3
-"""
-Flask app
-"""
-from flask import (
-    Flask,
-    render_template,
-    request
-)
+"""Module to Force locale with URL Parameter"""
+from flask import (render_template, request, Flask)
 from flask_babel import Babel
 
 
-class Config(object):
-    """
-    Configuration for Babel
-    """
+class Config:
+    """Configures available languages in our app"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-app.config.from_object(Config)
 babel = Babel(app)
+app.config.from_object(Config)
 
 
 @babel.localeselector
 def get_locale():
-    """
-    Select and return best language match based on supported languages
-    """
-    loc = request.args.get('locale')
-    if loc in app.config['LANGUAGES']:
-        return loc
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """Returns the locale requested via the 'locale' parameter"""
+    args = request.args.get("locale")
+    support_langs = ["en", "fr"]
+    if args is not None and args in supported_langs:
+        return args
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Handles / route
-    """
-    return render_template('4-index.html')
+@app.route("/")
+def display_basic_page():
+    """Displays a simple page to test Babel connection"""
+    return render_template("4-index.html")
 
 
 if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run()
